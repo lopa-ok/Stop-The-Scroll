25a720716173b4c000e35123900eb10207988bb9
philopater <lopa@programmer.net>
2024-03-24 19:13:19 +0200

    main

---
+++ README.md
@@ -0,0 +1,30 @@
+# Skip the Scroll
+
+
+
+Tired of scrolling through hundreds of github issue comments to find that one magical response? Skip the scroll is a simple chrome extension that sorts github issue comments for you and allows you to iterate through them based on their social reactions.
+
+
+## Prerequisites
+
+- [node + npm](https://nodejs.org/) (Current Version)
+
+## Setup
+
+```
+npm install
+```
+
+## Build
+
+```
+npm run build
+```
+
+## Build in watch mode
+
+### terminal
+
+```
+npm run watch
+```
---
+++ src/content_script.tsx
@@ -0,0 +1,89 @@
+let comments: CommentData[] = [];
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.type === "loadComments") {
+    const _comments = parseComments();
+    comments = _comments;
+    sendResponse(comments);
+    return true;
+  }
+  if (msg.type === "scrollTo") {
+    const commentData = comments[Number(msg.index)];
+    if (commentData) {
+      const y =
+        commentData.comment.getBoundingClientRect().top +
+        window.pageYOffset +
+        -128;
+      window.scrollTo({ top: y, behavior: "smooth" });
+      comments.forEach((comment) => {
+        // @ts-ignore
+        comment.comment.style = "box-shadow: 0px 0px 0px 0px";
+      });
+      // @ts-ignore
+      commentData.comment.style = "box-shadow: 0px 0px 0px 1.5px #2BA44E;";
+      sendResponse(commentData);
+      return true;
+    }
+  }
+  return true;
+});
+
+type CommentData = {
+  comment: Element;
+  upvotes: number;
+  timestamp: number;
+};
+
+const parseComments = () => {
+  const comments: CommentData[] = [];
+
+  // Get all posts on the page
+  const commentElements = document.getElementsByClassName(
+    "timeline-comment comment"
+  );
+
+  // All upvote emojis
+  const upvoteEmojis = ["👍", "😄", "🎉", "🚀", "❤️"];
+
+  Array.from(commentElements).forEach((comment) => {
+    const reactions = comment.getElementsByClassName(
+      "social-reaction-summary-item"
+    );
+    let totalUpvotes = 0;
+    Array.from(reactions).forEach((reaction) => {
+      // @ts-ignore
+      let splitText: string[] = reaction.innerText.split("\n");
+
+      let emoji = splitText[0];
+      let numReactions = Number(splitText[1]);
+
+      // Increase totalUpvotes if there are upvote emojis in the reaction
+      if (upvoteEmojis.includes(emoji)) {
+        totalUpvotes += numReactions;
+      }
+    });
+    const timestamp = Date.parse(
+      comment
+        .getElementsByClassName("js-timestamp")[0]
+        .children[0].getAttribute("datetime")!
+    );
+    comments.push({ comment, upvotes: totalUpvotes, timestamp: timestamp });
+  });
+
+  sortComments(comments);
+
+  return comments;
+};
+
+const sortComments = (comments: CommentData[]) => {
+  comments
+    .sort(function (x: CommentData, y: CommentData) {
+      var n = x.upvotes - y.upvotes;
+      if (n !== 0) {
+        return n;
+      }
+
+      return x.timestamp - y.timestamp;
+    })
+    .reverse();
+};
